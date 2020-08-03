fa67e6826c56c3a4a72871f7e08608d8aaf4d1d1
PatriceVang <patrice@vinova.com.sg>
2020-08-03 17:06:22 +0700

    add Project demo_call_api

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Project_Flutter
 - fluter_bloc: Demo counter
+- demo_call_api: Get Post method, parse json to list Object
---
+++ demo_call_api/lib/src/model/user.dart
@@ -0,0 +1,15 @@
+
+
+class User {
+  String name;
+  int id;
+  User({this.name, this.id});
+
+  factory  User.fromJson(Map<String, dynamic> json) {
+    return User(
+      name: json['name'],
+      id: json['id']
+    );
+  }
+
+}
