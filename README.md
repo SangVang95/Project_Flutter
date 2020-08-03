fd4636eb4c573ab1df5838d28f1e1f3d998ab537
PatriceVang <patrice@vinova.com.sg>
2020-08-03 14:44:38 +0700

    .

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Project_Flutter
 - fluter_bloc: Demo counter
-- listview_flutter_app: use Routes
--- listview_flutter_app/lib/flower.dart
+++
@@ -1,7 +0,0 @@
-class Flower{
-  final String name;
-  final String description;
-  final String imageURL;
-
-  const Flower(this.name, this.description, this.imageURL);
-}
